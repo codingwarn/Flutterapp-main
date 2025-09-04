b41a757375ff3278f085efa4931b4e1581523093
dhananjay shivaji pawade <dhananjayp.officail21@gmail.com>
2025-09-04 11:08:20 +0530

    second commit

--- lib/core/di/injection.config.dart
+++ lib/core/di/injection.config.dart
@@ -1,32 +1,43 @@
+// dart format width=80
 // GENERATED CODE - DO NOT MODIFY BY HAND
 
-import 'package:get_it/get_it.dart' as _i1;
-import 'package:injectable/injectable.dart' as _i2;
+// **************************************************************************
+// InjectableConfigGenerator
+// **************************************************************************
 
-import '../../data/datasources/photo_remote_data_source.dart' as _i4;
-import '../../data/repositories/photo_repository_impl.dart' as _i6;
-import '../../domain/repositories/photo_repository.dart' as _i5;
-import '../../domain/usecases/get_photos_usecase.dart' as _i7;
-import '../../presentation/blocs/photo/photo_bloc.dart' as _i8;
-import '../network/api_client.dart' as _i3;
+// ignore_for_file: type=lint
+// coverage:ignore-file
 
-_i1.GetIt init(
-  _i1.GetIt getIt, {
+// ignore_for_file: no_leading_underscores_for_library_prefixes
+import 'package:get_it/get_it.dart' as _i174;
+import 'package:injectable/injectable.dart' as _i526;
+
+import '../../data/datasources/photo_remote_data_source.dart' as _i538;
+import '../../data/repositories/photo_repository_impl.dart' as _i747;
+import '../../domain/repositories/photo_repository.dart' as _i236;
+import '../../domain/usecases/get_photos_usecase.dart' as _i997;
+import '../../presentation/blocs/photo/photo_bloc.dart' as _i1026;
+import '../network/api_client.dart' as _i557;
+
+// initializes the registration of main-scope dependencies inside of GetIt
+_i174.GetIt init(
+  _i174.GetIt getIt, {
   String? environment,
-  _i2.EnvironmentFilter? environmentFilter,
+  _i526.EnvironmentFilter? environmentFilter,
 }) {
-  final gh = _i2.GetItHelper(
+  final gh = _i526.GetItHelper(
     getIt,
     environment,
     environmentFilter,
   );
-  gh.singleton<_i3.ApiClient>(_i3.ApiClient());
-  gh.factory<_i4.PhotoRemoteDataSource>(
-      () => _i4.PhotoRemoteDataSourceImpl(gh<_i3.ApiClient>()));
-  gh.factory<_i5.PhotoRepository>(
-      () => _i6.PhotoRepositoryImpl(gh<_i4.PhotoRemoteDataSource>()));
-  gh.factory<_i7.GetPhotosUseCase>(
-      () => _i7.GetPhotosUseCase(gh<_i5.PhotoRepository>()));
-  gh.factory<_i8.PhotoBloc>(() => _i8.PhotoBloc(gh<_i7.GetPhotosUseCase>()));
+  gh.singleton<_i557.ApiClient>(() => _i557.ApiClient());
+  gh.factory<_i538.PhotoRemoteDataSource>(
+      () => _i538.PhotoRemoteDataSourceImpl(gh<_i557.ApiClient>()));
+  gh.factory<_i236.PhotoRepository>(
+      () => _i747.PhotoRepositoryImpl(gh<_i538.PhotoRemoteDataSource>()));
+  gh.factory<_i997.GetPhotosUseCase>(
+      () => _i997.GetPhotosUseCase(gh<_i236.PhotoRepository>()));
+  gh.factory<_i1026.PhotoBloc>(
+      () => _i1026.PhotoBloc(gh<_i997.GetPhotosUseCase>()));
   return getIt;
 }
